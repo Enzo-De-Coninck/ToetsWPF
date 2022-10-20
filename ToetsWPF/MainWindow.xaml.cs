e9203d21f000b5ed9d9cd1d7084a077ec01c1004
Enzo <enzo.deconinck@vdabcampus.be>
2022-10-20 14:56:21 +0200

    opslaan en openen is nog niet perfect

--- ToetsWPF/MainWindow.xaml.cs
+++ ToetsWPF/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Reflection;
 using System.ComponentModel;
 using System.IO;
+using Microsoft.Win32;
 
 namespace ToetsWPF
 {
@@ -71,6 +72,8 @@ namespace ToetsWPF
             Uri imageUri = new Uri(stringPath, UriKind.Relative);
             BitmapImage imageBitmap = new BitmapImage(imageUri);
             deAchtergrond.ImageSource = imageBitmap;
+            KaartOpslaan.IsEnabled = true;
+            KaartAfdrukken.IsEnabled = true;
         }
 
         private void Geboorte_Click(object sender, RoutedEventArgs e)
@@ -80,6 +83,8 @@ namespace ToetsWPF
             Uri imageUri = new Uri(stringPath, UriKind.Relative);
             BitmapImage imageBitmap = new BitmapImage(imageUri);
             deAchtergrond.ImageSource = imageBitmap;
+            KaartOpslaan.IsEnabled = true;
+            KaartAfdrukken.IsEnabled = true;
         }
 
         private void Nieuw()
@@ -87,6 +92,8 @@ namespace ToetsWPF
             DeStackPanel.Visibility = Visibility.Hidden;
             KaartAfdrukken.IsEnabled = false;
             KaartOpslaan.IsEnabled = false;
+            ComboBoxKleuren.SelectedIndex = 0;
+            ComboBoxLettertype.SelectedIndex = 0;
             
         }
 
@@ -105,6 +112,113 @@ namespace ToetsWPF
         {
             if (MessageBox.Show("Wilt u het programma afsluiten ?", "Afsluiten", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.No)
                 e.Cancel = true;
+        }
+
+        private Ellipse sleepbal = new Ellipse();
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            sleepbal = (Ellipse)sender;
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject sleepKleur = new DataObject("deKleur", sleepbal.Fill);
+                DragDrop.DoDragDrop(sleepbal, sleepKleur, DragDropEffects.Move);
+            }
+        }
+
+        
+        private void Ellipse_Drop(object sender, DragEventArgs e)
+        {
+            
+            if (e.Data.GetDataPresent("deKleur"))
+            {
+
+                Brush gesleepteKleur = (Brush)e.Data.GetData("deKleur");
+                Ellipse bal = new Ellipse();
+                bal.Fill = gesleepteKleur;
+                bal.Height = 40;
+                bal.Width = 40;
+                bal.Stroke = Brushes.Black;
+                bal.StrokeThickness = 5;
+                Point punt = e.GetPosition(deCanvas);
+                double posX = punt.X -20;
+                double posY = punt.Y -20;
+                Canvas.SetLeft(bal, posX);
+                Canvas.SetTop(bal, posY);
+                deCanvas.Children.Add(bal);
+            }
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.FileName = "Wenskaart";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "Wenskaarten |*.txt";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamReader bestand = new StreamReader(dlg.FileName))
+                    {
+                        deAchtergrond.ImageSource = new ImageSourceConverter().ConvertFromString(bestand.ReadLine()) as ImageSource;
+                        int tijdelijk = int.Parse(bestand.ReadLine());
+                        for (int i = 0; i < tijdelijk; i++)
+                        {
+                            Ellipse bol = new Ellipse();
+                            bol.Fill = (SolidColorBrush)new BrushConverter().ConvertFromString(bestand.ReadLine());
+                            double canvasLeft = Convert.ToDouble(bestand.ReadLine());
+                            double canvasTop = Convert.ToDouble(bestand.ReadLine());
+                            Canvas.SetLeft(bol, canvasLeft);
+                            Canvas.SetTop(bol, canvasTop);
+                        }
+                        TextBoxWens.Text = Console.ReadLine();
+                        TypeConverter convertLettertype = TypeDescriptor.GetConverter(typeof(FontFamily));
+                        TextBoxWens.FontFamily = (FontFamily)convertLettertype.ConvertFromString(bestand.ReadLine());
+                        TextBoxWens.FontSize = Convert.ToDouble(bestand.ReadLine());    
+
+                        Bestandslocatie.Content = System.IO.Path.GetFullPath(dlg.FileName).ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("openen mislukt: " + ex.Message);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.FileName = "Wenskaart";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "Wenskaarten |*.txt";
+                if (dlg.ShowDialog() == true)
+                {
+                    using (StreamWriter bestand = new StreamWriter(dlg.FileName))
+                    {
+                        bestand.WriteLine(deAchtergrond.ImageSource.ToString());
+                        bestand.WriteLine(deCanvas.Children.Count);
+                        foreach (Ellipse bol in deCanvas.Children)
+                        {
+                            bestand.WriteLine(bol.Fill.ToString());
+                            bestand.WriteLine(Canvas.GetLeft(bol));
+                            bestand.WriteLine(Canvas.GetTop(bol));
+                        }
+                        bestand.WriteLine(TextBoxWens.Text.ToString());
+                        bestand.WriteLine(TextBoxWens.FontFamily.ToString());
+                        bestand.WriteLine(TextBoxWens.FontSize.ToString());
+                        Bestandslocatie.Content = System.IO.Path.GetFullPath(dlg.FileName).ToString();
+                    }
+                }
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("opslaan mislukt: " + ex.Message);
+            }
         }
     }
 }
